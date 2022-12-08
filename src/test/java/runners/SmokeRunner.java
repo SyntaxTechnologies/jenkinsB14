e779f70218f2ee46987538d06287c92b640bd201
sohai <SyntaxTechnologies>
2022-12-08 07:06:48 +0530

    LC07122022

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         //when you set dry run to true, it stops actual execution
         //it will quickly scan all the gherkin steps whether they are implemented or not
         //when we set dry run to false, it starts execution again
-         dryRun = true,
+         dryRun = false,
         tags = "@sprint4",
         //to remove irrelavant information from console, you need to set monochrome to true
         monochrome = true,
