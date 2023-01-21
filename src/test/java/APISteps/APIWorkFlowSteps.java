cff12ddfbcb0370681b5a3ef5e84bbdd0791800f
sohai <SyntaxTechnologies>
2023-01-21 22:59:28 +0530

    lc

--- src/test/java/APISteps/APIWorkFlowSteps.java
+++ src/test/java/APISteps/APIWorkFlowSteps.java
@@ -5,6 +5,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import utils.APIConstants;
+import utils.APIPayloadConstant;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,28 +17,21 @@ public class APIWorkFlowSteps {
     @Given("a request is prepared for creating an employee")
     public void a_request_is_prepared_for_creating_an_employee() {
          request = given().
-                header("Content-Type", "application/json").
-                header("Authorization", GenerateTokenSteps.token).
-                body("{\n" +
-                        "  \"emp_firstname\": \"sara\",\n" +
-                        "  \"emp_lastname\": \"bou\",\n" +
-                        "  \"emp_middle_name\": \"ms\",\n" +
-                        "  \"emp_gender\": \"F\",\n" +
-                        "  \"emp_birthday\": \"2011-01-12\",\n" +
-                        "  \"emp_status\": \"confirmed\",\n" +
-                        "  \"emp_job_title\": \"QA Engineer\"\n" +
-                        "}");
+                header(APIConstants.Header_Key_Content_Type, APIConstants.Header_Value_Content_Type).
+                header(APIConstants.Header_Key_Authorization, GenerateTokenSteps.token).
+                body(APIPayloadConstant.createEmployeePayload());
     }
 
     @When("a POST call is made to create an employee")
     public void a_post_call_is_made_to_create_an_employee() {
-         response = request.when().post("/createEmployee.php");
+         response = request.when().post(APIConstants.CREATE_EMPLOYEE_URI);
          response.prettyPrint();
     }
 
     @Then("the status code for creating an employee is {int}")
-    public void the_status_code_for_creating_an_employee_is(Integer int1) {
-        response.then().assertThat().statusCode(int1);
+    public void the_status_code_for_creating_an_employee_is(Integer sholpan) {
+        response.then().assertThat().statusCode(sholpan);
     }
+
 
 }
---
+++ src/test/java/utils/APIPayloadConstant.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class APIPayloadConstant {
+
+    public static String createEmployeePayload(){
+        String createEmployeePayload =
+                "{\n" +
+                        "  \"emp_firstname\": \"sara\",\n" +
+                        "  \"emp_lastname\": \"bou\",\n" +
+                        "  \"emp_middle_name\": \"ms\",\n" +
+                        "  \"emp_gender\": \"F\",\n" +
+                        "  \"emp_birthday\": \"2011-01-12\",\n" +
+                        "  \"emp_status\": \"confirmed\",\n" +
+                        "  \"emp_job_title\": \"QA Engineer\"\n" +
+                        "}";
+        return createEmployeePayload;
+    }
+
+
+}
